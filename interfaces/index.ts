ec71e79bd27ad3267619aead62d9b68a96cff1ff
Emnet Befkadu <162457277+Emnet19@users.noreply.github.com>
2025-10-06 07:34:19 +0300

    Create index.ts

---
+++ interfaces/index.ts
@@ -0,0 +1,12 @@
+export interface PropertyProps{
+  name:string;
+  address:{
+    city:string;
+    country:string;
+  }
+  category:string[];
+  rating:string;
+  image:string;
+  description:string;
+  price:number;
+}
